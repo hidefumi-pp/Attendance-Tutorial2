2a1c352f05c94e476cd053b26c0cb9f2845a4c1a
EC2 Default User <ec2-user@ip-172-31-24-72.ap-southeast-1.compute.internal>
2019-08-18 08:49:45 +0000

    Finish japanization-and-format

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -13,8 +14,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
   def destroy
-    #ログイン中の場合のみログアウト処理を実行します。
+     # ログイン中の場合のみログアウト処理を実行します。
     log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user,only: [:index, :show, :edit, :update, :destroy]
-  before_action :correct_user, only:[:edit,:update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
 
   def index
-    @users = User.paginate(page:params[:page])
+    @users = User.paginate(page: params[:page])
   end
-  
+
   def show
   end
 
@@ -19,24 +18,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user #保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "ユーザー情報を更新しました"
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
 
@@ -46,27 +44,26 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
-    #beforeフィルター
-    
-    #paramsハッシュからユーザーを取得します
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
-    
-    #ログイン済みのユーザーか確認します
+
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
-       store_location
-       flash[:danger] = "ログインしてください"
-       redirect_to login_url
-     end
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
     end
 
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
@@ -74,10 +71,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    #システム管理権限所有かどうか判定します。
+    # システム管理権限所有かどうか判定します。
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
 end
-
-
